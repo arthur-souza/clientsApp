8cfcfba3f366985b249310db817f81efff9e787a
Arthur Souza <arthur@arthursouza.com.br>
2018-12-11 23:08:58 -0300

    Criando activity de clientes e linkando com a tela principal

--- app/src/main/java/br/com/arthursouza/clients/MainActivity.kt
+++ app/src/main/java/br/com/arthursouza/clients/MainActivity.kt
@@ -11,11 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* Adicionar um evento no botão de cadastrar para abir uma nova tela */
+        /* Adicionar um evento no botão de cadastrar para abrir a tela de cadastro */
         val btnRegister : Button = this.findViewById(R.id.btn_register)
         btnRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity ::class.java)
             startActivity(intent)
+        }
+
+        /* Adicionar um evento no botão de cadastrar para abrir a tela de clientes */
+        val btnClients : Button = this.findViewById(R.id.btn_clients)
+        btnClients.setOnClickListener {
+            val itent = Intent(this, ClientsActivity ::class.java)
+            startActivity(itent)
         }
     }
 }
