8914de0f5acb8a4155be80c5156e5c438701baa0
Arthur Souza <arthur@arthursouza.com.br>
2018-12-11 23:01:34 -0300

    Criando activity de registro e linkando com a tela principal

--- app/src/main/java/br/com/arthursouza/clients/MainActivity.kt
+++ app/src/main/java/br/com/arthursouza/clients/MainActivity.kt
@@ -1,12 +1,21 @@
 package br.com.arthursouza.clients
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Adicionar um evento no botão de cadastrar para abir uma nova tela */
+        val btnRegister : Button = this.findViewById(R.id.btn_register)
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity ::class.java)
+            startActivity(intent)
+        }
     }
 }
